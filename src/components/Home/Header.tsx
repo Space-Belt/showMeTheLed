ce4bae5194f11e2261c34ce6512af51d0f192f71
OnlyWooo <dngurwn@gmail.com>
2024-06-13 19:46:04 +0900

    헤더 컴포넌트화

---
+++ src/components/Home/Header.tsx
@@ -0,0 +1,33 @@
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import Setting from '../../assets/icons/settingIcon.svg';
+import {COLORS} from '../../theme/theme';
+
+type Props = {
+  handleSettingBtn: () => void;
+};
+
+const Header = ({handleSettingBtn}: Props) => {
+  return (
+    <View style={styles.headerWrapper}>
+      <TouchableOpacity onPress={handleSettingBtn}>
+        <Setting style={styles.settingIcon} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  headerWrapper: {
+    paddingVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  settingIcon: {
+    width: 25,
+    height: 25,
+    color: COLORS.SECOND,
+  },
+});
